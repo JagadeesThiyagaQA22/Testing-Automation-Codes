931c5d8b5748fbd409beae45cc7170272e457a4b
maharajan_k <maharajan_k@OTLCHELP282.otlchennai.com>
2022-05-17 18:00:05 +0530

    Second Commit

---
+++ Java/src/Testing/Test1.java
@@ -0,0 +1,25 @@
+package Testing;
+
+public class Test1 {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Welocome...!");
+	        int arr[]={34,25,23,58,45,17,18,21,46,533};
+	        int big=arr[0];
+	        int sec_big=arr[0];
+	        for(int i=1;i<arr.length;i++)
+	        {
+	            if(arr[i]>big)
+	                big=arr[i];
+	        }
+	        for(int i=1;i<arr.length;i++)
+	        {
+	            if(arr[i]>sec_big && arr[i]<big)
+	                sec_big=arr[i];
+	        }
+	        System.out.println("second big number is: "+sec_big);
+	    }
+	}
+
+
